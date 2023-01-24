26d4981eef4c3a705e1477e780da974cc6e04ceb
Cherniugov Yevgenii <cherniugov@gmail.com>
2023-01-24 22:34:14 +0200

    Updates

---
+++ static/js/647.04da17c7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05=self.webpackChunkgoit_react_hw_05||[]).push([[647],{647:function(e,s,n){n.r(s),n.d(s,{default:function(){return l}});n(791);var a=n(87),t=n(689),r="style_navBar__w-qZ6",c="style_navLink__NQSwR",i=n(184);var l=function(){return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsxs)("nav",{className:r,children:[(0,i.jsx)(a.OL,{className:c,to:"/",children:"Home"}),(0,i.jsx)(a.OL,{className:c,to:"/movies",children:"Movies"})]}),(0,i.jsx)(t.j3,{})]})}}}]);
+//# sourceMappingURL=647.04da17c7.chunk.js.map
